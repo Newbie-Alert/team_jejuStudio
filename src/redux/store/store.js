4c26823a83447c5deabfc19b5e1ecb885120d68a
Rock Kyun <mjnn100801@naver.com>
2023-12-06 00:39:25 +0900

    ✨ update :  사전조사 페이지 구현, 로그인/회원가입 구현, 매칭 페이지로 이동하도록

---
+++ src/redux/store/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authSlice from "../modules/authSlice";
+
+const store = configureStore({
+  reducer: {
+    authSlice
+  }
+})
+
+
+export default store;
--- src/router/Router.js
+++ src/router/Router.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import MainPage from "../pages/MainPage"
 import GlobalStyle from "../globalStyle/GlobalStyle"
+import Question from "../pages/Question/Question"
+import Login from "../pages/Login/Login"
 
 
 const Router = () => {
@@ -9,6 +11,9 @@ const Router = () => {
       <GlobalStyle />
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path="/question" element={<Question />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/photogrpher" element={<Login />} />
       </Routes>
     </BrowserRouter>
   )
