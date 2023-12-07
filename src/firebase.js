7ed926444528f915fe4893b320f59462225917b1
Rock Kyun <mjnn100801@naver.com>
2023-12-07 21:26:10 +0900

    테스트

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-analytics.js";
 import { getAuth } from 'firebase/auth'
 import config from "./config";
 
@@ -10,3 +11,4 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 
 export default app
+
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import KAKAO from '../components/map/Kakao';
 import styled from 'styled-components';
+import Header from '../components/Layout/Header';
+import Introduction from '../components/home/Introduction';
+import MainBanner from '../components/home/MainBanner';
 
 const MapContainer = styled.div`
   width: 100%;
@@ -10,8 +13,22 @@ const MapContainer = styled.div`
 
 export default function MainPage() {
   return (
-    <MapContainer>
-      <KAKAO />
-    </MapContainer>
+    <div>
+      <stContainer>
+        <Header />
+        <MainBanner />
+        <Introduction />
+        <MapContainer>
+          <KAKAO />
+        </MapContainer>
+      </stContainer>
+    </div>
   );
 }
+
+const stContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  align-items: center;
+  flex-direction: column;
+`;
