c5155f166bcaacd229e1c626422dabd704bf4cd9
Rock Kyun <mjnn100801@naver.com>
2023-12-11 11:27:10 +0900

    헤더 로그인/로그아웃 추가

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,15 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router';
 import * as St from './HeaderStyles';
+import { useDispatch, useSelector } from 'react-redux';
+import { __doLogout } from '../../redux/modules/authSlice';
 
 function Header() {
   // STATES
   const [scrollPosition, setScrollPosition] = useState('0');
   const [isModalOn, setIsModalOn] = useState(false);
+  const { isLogin } = useSelector((state) => state.authSlice);
 
   // HOOKS
   const navi = useNavigate('/question');
   const { pathname } = useLocation();
+  const dispatch = useDispatch();
 
   // FUNCTIONS
   const updateScroll = () => {
@@ -26,6 +30,10 @@ function Header() {
 
   const modalToggle = () => {
     setIsModalOn((prev) => !prev);
+  };
+
+  const logout = () => {
+    dispatch(__doLogout());
   };
 
   // USE EFFECT
@@ -56,6 +64,17 @@ function Header() {
                 <button onClick={() => navigateTo('/question')}>예약하기</button>
                 <button onClick={() => navigateTo('/profile')}>작가소개</button>
                 <button onClick={() => navigateTo('/portfolio')}>포트폴리오</button>
+                <button
+                  onClick={() => {
+                    if (isLogin === false) {
+                      navi('/login');
+                    } else {
+                      logout();
+                    }
+                  }}
+                >
+                  {isLogin ? '로그아웃' : '로그인'}
+                </button>
               </St.StHeaderButton>
             </St.StHeader>
           </St.StWrapper>
@@ -76,6 +95,17 @@ function Header() {
                 <button onClick={() => navigateTo('/question')}>예약하기</button>
                 <button onClick={() => navigateTo('/profile')}>작가소개</button>
                 <button onClick={() => navigateTo('/portfolio')}>포트폴리오</button>
+                <button
+                  onClick={() => {
+                    if (isLogin === false) {
+                      navi('/login');
+                    } else {
+                      logout();
+                    }
+                  }}
+                >
+                  {isLogin ? '로그아웃' : '로그인'}
+                </button>
               </St.ChangeStHeaderButton>
             </St.ChangeStHeader>
           </St.ChangeStWrapper>
--- src/redux/modules/authSlice.js
+++ src/redux/modules/authSlice.js
@@ -7,7 +7,8 @@ const initialState = {
   user: [],
   isLoading: false,
   isError: false,
-  error: null
+  error: null,
+  isLogin: !!localStorage.getItem('user'),
 }
 
 // THUNK FUNCTIONS
@@ -36,8 +37,19 @@ export const __doLogin = createAsyncThunk(
       const res = await signInWithEmailAndPassword(auth, email, password);
       const { accessToken, uid } = res.user
       const data = { accessToken, uid }
-
       return thunkAPI.fulfillWithValue(data)
+    }
+    catch (err) {
+      return thunkAPI.rejectWithValue(err.code)
+    }
+  }
+)
+
+export const __doLogout = createAsyncThunk(
+  "LOGOUT",
+  async (payload, thunkAPI) => {
+    try {
+      return thunkAPI.fulfillWithValue(null);
     }
     catch (err) {
       return thunkAPI.rejectWithValue(err.code)
@@ -80,9 +92,29 @@ const authSlice = createSlice({
         state.isLoading = false;
         state.isError = false;
         state.user = action.payload;
+        state.isLogin = true;
         localStorage.setItem('user', JSON.stringify(action.payload))
       })
       .addCase(__doLogin.rejected, (state, action) => {
+        state.isError = true;
+        state.error = action.payload;
+        state.isLoading = false
+      })
+
+    // LOGOUT
+    builder
+      .addCase(__doLogout.pending, (state, action) => {
+        state.isLoading = true;
+        state.isError = false;
+      })
+      .addCase(__doLogout.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.user = [];
+        state.isLogin = false;
+        localStorage.clear();
+      })
+      .addCase(__doLogout.rejected, (state, action) => {
         state.isError = true;
         state.error = action.payload;
         state.isLoading = false
