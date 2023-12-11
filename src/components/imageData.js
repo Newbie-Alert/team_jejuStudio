73f2faa2ca2175f04efb44ee6ffd268347167ba6
Rock Kyun <mjnn100801@naver.com>
2023-12-11 09:42:44 +0900

    메인 배너 이미지 파일 수정

--- src/components/imageData.js
+++ src/components/imageData.js
@@ -1,29 +1,29 @@
 
 const imageData = [
     {
-        label : "Image 1",
-        alt : "image1",
-        url : "assets/bg1.jpg",
+        label: "Image 1",
+        alt: "image1",
+        url: "/assets/bg1.jpg",
     },
     {
-        label : "Image 2",
-        alt : "image2",
-        url : "assets/bg2.jpg",
+        label: "Image 2",
+        alt: "image2",
+        url: "/assets/bg2.jpg",
     },
     {
-        label : "Image 3",
-        alt : "image3",
-        url : "assets/bg3.jpg",
+        label: "Image 3",
+        alt: "image3",
+        url: "/assets/bg3.jpg",
     },
     {
-        label : "Image 4",
-        alt : "image4",
-        url : "assets/bg4.jpg",
+        label: "Image 4",
+        alt: "image4",
+        url: "assets/bg4.jpg",
     },
     {
-        label : "Image 5",
-        alt : "image5",
-        url : "assets/bg5.jpg",
+        label: "Image 5",
+        alt: "image5",
+        url: "assets/bg5.jpg",
     },
 
 ];
